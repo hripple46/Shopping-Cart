20824b9c7478af860ad680bd2bdc0bcab90d73eb
Henry Ripple <hripple46@gmail.com>
2023-03-10 15:38:13 -0600

    added state for two example items

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 let ShoppingCart = () => {
+  let [items, setItems] = useState([
+    { name: "Banana", qty: 0 },
+    { name: "Apple", qty: 0 },
+  ]);
+  let displayInventory = () => {
+    return items.map((item) => {
+      return (
+        <div key={item.name}>
+          <h1>{item.name}</h1>
+          <h3>{item.qty}</h3>
+        </div>
+      );
+    });
+  };
   return (
     <div>
       <h1>Welcome to Your Shopping Cart</h1>
+      {displayInventory()}
       <Link to="/">Home Page</Link>
     </div>
   );
