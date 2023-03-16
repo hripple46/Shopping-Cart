8099d5187180fa6e427defe71d1cf38f9ae6dd4e
Henry Ripple <hripple46@gmail.com>
2023-03-16 17:03:01 -0500

    added decrement function that will not decrease if qty is <=0

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -12,7 +12,9 @@ let ShoppingCart = () => {
         <div key={item.name}>
           <h1>{item.name}</h1>
           <div className="itemCounter">
-            <button id={item.name}>-</button>
+            <button onClick={decrement} id={item.name + "Dec"}>
+              -
+            </button>
             <h3>{item.qty}</h3>
             <button onClick={increment} id={item.name}>
               +
@@ -26,6 +28,16 @@ let ShoppingCart = () => {
     let newItems = items.map((item) => {
       if (item.name === e.target.id) {
         return { name: item.name, qty: item.qty + 1 };
+      } else {
+        return item;
+      }
+    });
+    setItems(newItems);
+  };
+  let decrement = (e) => {
+    let newItems = items.map((item) => {
+      if (item.name + "Dec" === e.target.id && item.qty > 0) {
+        return { name: item.name, qty: item.qty - 1 };
       } else {
         return item;
       }
