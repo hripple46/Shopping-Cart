0c5c5ddd32f96cff3a23f614b2ae7dd6ac718470
Henry Ripple <hripple46@gmail.com>
2023-05-10 10:59:07 -0500

    updated readme file

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as RIPDb } from "./img/RIPDb_X.svg";
 
 let HomePage = () => {
   return (
     <div>
       <h1>Welcome to Your Home Page</h1>
+      <RIPDb />
       <Link to="/shoppingcart">Shopping Cart</Link>
     </div>
   );
